fefa60d20cdc3d3d3f3533e06aeaf907fe098c3b
Cheesey990 <james.cheeseman@barnett-waddingham.co.uk>
2020-08-20 11:15:40 +0100

    Added in documentation for users

--- DDPShiny/ui.R
+++ DDPShiny/ui.R
@@ -8,6 +8,10 @@ shinyUI(fluidPage(
         
             sidebarPanel(
             
+                h3("Documentation instructions:"),
+                h4("This model estimates how long teeth in guinea pigs could grow if you add dosages of Vitamin C. 
+                   Use the slider to state the dosage amount and see the estimated tooth length."),
+                
                 sliderInput("sliderDose", "What is the dose of the Vitamin C (mg per day)?", 0, 3, value = 1, step = 0.1),
                 
                 ),
